25fa9bfb2cf5af98fb609402da30ae82f8bdc306
Aamir <aamirasaram2107@gmail.com>
2023-10-18 13:48:59 +0200

    Correct rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence:true
-  validates :email, presence:true
-  validates :password, presence:true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
