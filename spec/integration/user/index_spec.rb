166270e5cb123a443cc73bc231ca2b2296ff498c
Aamir <aamirasaram2107@gmail.com>
2023-10-18 13:02:14 +0200

    Add splash screen/login/sign up integration spec

---
+++ spec/integration/user/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Splash screen', type: :feature do
+  let(:user) { User.create(name: 'John', email: 'test@test.com', password: '121212') }
+
+  describe 'Testing integration specs for users index page' do
+    before do
+      visit '/'
+    end
+
+    it 'should display the splash screen' do
+      expect(page).to have_content('Centsible')
+    end
+
+    it 'clicking on the sign up button should take you to the sign up page' do
+      click_on('Sign Up')
+      expect(page).to have_content('Sign up')
+    end
+
+    it 'clicking on the log in button should take you to the log in page' do
+      click_on('Log In')
+      expect(page).to have_content('Login')
+    end
+
+    it 'signing up should take you to the categories index page' do
+      click_on('Sign Up')
+      fill_in('Full name', with: 'John')
+      fill_in('Email', with: 'john@gmail.com')
+      fill_in('Password', with: '121212')
+      fill_in('Confirm Password', with: '121212')
+      click_on('Sign Up')
+      expect(page).to have_content('CATEGORIES')
+    end
+  end
+end
